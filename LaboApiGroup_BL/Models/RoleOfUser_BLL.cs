577eb03fac9aa828574a44ad7d31f078280eab06
m.buyens <michaelbuyens@live.com>
2022-10-13 16:30:44 +0200

    Créations des entités et models

---
+++ LaboApiGroup_BL/Models/RoleOfUser_BLL.cs
@@ -0,0 +1,16 @@
+﻿using LaboApiGroup_Repo.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboApiGroup_BL.Models
+{
+    public class RoleOfUser_BLL : IEntity<int> 
+    {
+        public int Id { get; set; }
+        public int Id_User { get; set; }
+        public int Id_Role { get; set; }
+    }
+}
---
+++ LaboApiGroup_Repo/Entities/Users.cs
@@ -0,0 +1,17 @@
+﻿using LaboApiGroup_Repo.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboApiGroup_Repo.Entities
+{
+    public class Users : IEntity<int>
+    {
+        public int Id { get; set; }
+        public string NickName { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
