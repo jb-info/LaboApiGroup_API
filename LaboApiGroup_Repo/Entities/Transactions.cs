439b4970dc1bbe5d66f7ec812d27d8642b8b8294
thedeStroyer123A <110990247+thedeStroyer123A@users.noreply.github.com>
2022-10-13 16:05:12 +0200

    creations des entités

---
+++ LaboApiGroup_Repo/Entities/Transactions.cs
@@ -0,0 +1,19 @@
+﻿using LaboApiGroup_Repo.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaboApiGroup_Repo.Entities
+{
+    public class Transactions :IEntity<int>
+    {        
+        public int Id { get; set; }
+        public int Id_Project { get; set; }
+        public int Id_User { get; set; }
+        public int Amount  { get; set; }
+        public DateTime Date_Transaction { get; set; }
+        
+    }
+}
