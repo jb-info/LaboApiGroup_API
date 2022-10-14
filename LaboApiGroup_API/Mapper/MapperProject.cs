3a71840f446bf173a7d9f6c90fcdd289db92c8e8
Jérémy Bouillon <j.bouillon@stag.technofuturtic.education>
2022-10-14 09:36:00 +0200

    Mise en place de la methode d'extension sur B_Mika

---
+++ LaboApiGroup_API/Mapper/MapperProject.cs
@@ -0,0 +1,16 @@
+﻿using LaboApiGroup_API.Models;
+using LaboApiGroup_BL.Models;
+
+namespace LaboApiGroup_API.Mapper
+{
+    public static class MapperProject
+    {
+        public static Project_C ProjectBLToClient(this Project_BLL p_BLL)
+        {
+            return new Project_C()
+            {
+                Id = p_BLL.Id,
+            };
+        }
+    }
+}
